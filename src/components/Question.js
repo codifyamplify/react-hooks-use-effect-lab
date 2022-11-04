1e63f277053355fca993672c8fcd9e5baa5c3ff7
David Whitney <codifyamplify@gmail.com>
2022-11-04 15:52:52 -0400

    commit tree 1a395f2e4cb89a0616308935a8d457f7a0a0e66e

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+
+  console.log(`question ${question.id} answered!`)
+
+
+  useEffect(() => {
+    // reset timeRemaining state to 10 for the next question
+    if (timeRemaining === 0) {
+      setTimeRemaining(10)
+      onAnswered(false)
+      return
+    }
+    // setup a countdown / timeout useEffect 1 second
+    const timer = setTimeout(() => {
+      setTimeRemaining((timeRemaining) => timeRemaining - 1)
+    }, 1000)
+    // cleanup function to end side effect useEffect
+    return function cleanup(){
+      clearTimeout(timer)
+    }
+  }, [onAnswered, timeRemaining])
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
